94435367482d90f1405849b347852856641ed50c
Jeffrey Starke <dev@jeffreystarke.me>
2024-12-02 19:52:59 -0500

    Improvements to day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,34 +2,29 @@ import kotlin.math.abs
 
 fun main() {
     val input = readInput("Day01")
-    println("part 1 result = ${part1(input)}")
-    println("part 2 result = ${part2(input)}")
+    val (leftColumn, rightColumn) = input.splitToLeftAndRightColumns()
+    println("part 1 result = ${part1(leftColumn, rightColumn)}")
+    println("part 2 result = ${part2(leftColumn, rightColumn)}")
 }
 
-private fun part1(input: List<String>): Int {
-    val (leftList, rightList) = input.splitToLeftAndRightList()
-    val sortedLeftList = leftList.sorted()
-    val sortedRightList = rightList.sorted()
-    val idPairs = input.indices.map { LocationIdPair(sortedLeftList[it], sortedRightList[it]) }
-    return idPairs.sumOf { it.distance }
+private fun part1(leftColumn: List<Int>, rightColumn: List<Int>): Int {
+    return leftColumn.sorted().zip(rightColumn.sorted())
+        .map { (leftValue, rightValue) -> LocationIdPair(leftValue, rightValue) }
+        .sumOf { it.distance }
 }
 
-private fun part2(input: List<String>): Int {
-    val (leftList, rightList) = input.splitToLeftAndRightList()
-    val totalSimilarityScore = leftList.sumOf { leftValue ->
-        leftValue * rightList.count { it == leftValue }
+private fun part2(leftColumn: List<Int>, rightColumn: List<Int>): Int {
+    val frequenciesByValue = rightColumn.groupingBy { it }.eachCount()
+    val totalSimilarityScore = leftColumn.sumOf { leftValue ->
+        leftValue * frequenciesByValue.getOrDefault(leftValue, 0)
     }
     return totalSimilarityScore
 }
 
-private fun List<String>.splitToLeftAndRightList(): Pair<List<Int>, List<Int>> {
-    val leftList = mutableListOf<Int>()
-    val rightList = mutableListOf<Int>()
-    this.forEach { line ->
-        leftList.add(line.substringBefore(" ").toInt())
-        rightList.add(line.substringAfterLast(" ").toInt())
-    }
-    return Pair(leftList, rightList)
+private fun List<String>.splitToLeftAndRightColumns(): Pair<List<Int>, List<Int>> {
+    return this
+        .map { line -> line.substringBefore(" ").toInt() to line.substringAfterLast(" ").toInt() }
+        .unzip()
 }
 
 private data class LocationIdPair(val id1: Int, val id2: Int) {
