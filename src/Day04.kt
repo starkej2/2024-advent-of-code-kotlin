fcb4ffa291466d4ddbfdc7b5bea364f0516eb6e6
Jeffrey Starke <dev@jeffreystarke.me>
2024-12-04 21:02:24 -0500

    Day 4 part 1 improvements

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,73 +1,44 @@
 fun main() {
 //    val input = readInput("Day04")
-    val input = readInput("Test")
-    println("[part 1] result = ${part1(input)}")
-    println("[part 2] result = ${part2(input)}")
+    val grid = readInput("Test").map { lines -> lines.toCharArray() }
+    println("[part 1] result = ${part1(grid)}")
+    println("[part 2] result = ${part2(grid)}")
 }
 
-private fun part1(input: List<String>): Int {
-    val grid: List<CharArray> = input.map { lines -> lines.toCharArray() }
-    printMatrixWithCoordinates(grid) // WIP clean up
+private fun part1(grid: List<CharArray>): Int {
+    return grid.countOccurrencesOf("XMAS")
+}
 
+private fun List<CharArray>.countOccurrencesOf(
+    word: String,
+): Int {
     var count = 0
-    val searchWord = "XMAS"
-    grid.forEachIndexed { rowIndex, row ->
+    this.forEachIndexed { rowIndex, row ->
         row.forEachIndexed { columnIndex, letter ->
-            if (letter == searchWord.first()) {
-                // WIP let's improve this
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.UP)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.UP_RIGHT)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.RIGHT)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.DOWN_RIGHT)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.DOWN)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.DOWN_LEFT)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.LEFT)) {
-                    count++
-                }
-                if (grid.wordExists(searchWord, rowIndex, columnIndex, CrawlDirection.UP_LEFT)) {
-                    count++
+            val currentPosition = GridPosition(rowIndex, columnIndex)
+            if (letter == word.first()) {
+                CrawlDirection.entries.forEach { direction ->
+                    if (this.wordExists(word, currentPosition, direction)) {
+                        count++
+                    }
                 }
             }
         }
     }
-
     return count
 }
 
 private fun List<CharArray>.wordExists(
     word: String,
-    startRowIndex: Int,
-    startColumnIndex: Int,
+    startPosition: GridPosition,
     crawlDirection: CrawlDirection,
 ): Boolean {
     for (i in word.indices) {
-        // WIP can this be moved to the enum?
-        val nextColumnIndex = when (crawlDirection) {
-            CrawlDirection.UP, CrawlDirection.DOWN -> startColumnIndex
-            CrawlDirection.RIGHT, CrawlDirection.UP_RIGHT, CrawlDirection.DOWN_RIGHT -> startColumnIndex + i
-            CrawlDirection.LEFT, CrawlDirection.UP_LEFT, CrawlDirection.DOWN_LEFT -> startColumnIndex - i
-        }
-
-        val nextRowIndex = when (crawlDirection) {
-            CrawlDirection.UP, CrawlDirection.UP_RIGHT, CrawlDirection.UP_LEFT -> startRowIndex - i
-            CrawlDirection.RIGHT, CrawlDirection.LEFT -> startRowIndex
-            CrawlDirection.DOWN, CrawlDirection.DOWN_RIGHT, CrawlDirection.DOWN_LEFT -> startRowIndex + i
-        }
+        val nextColumnIndex = crawlDirection.calculatePosition(startPosition, i).column
+        val nextRowIndex = crawlDirection.calculatePosition(startPosition, i).row
 
         if ((nextRowIndex < 0 || nextRowIndex > this.lastIndex) ||
-            (nextColumnIndex < 0 || nextColumnIndex > this[startRowIndex].lastIndex)
+            (nextColumnIndex < 0 || nextColumnIndex > this[startPosition.row].lastIndex)
         ) {
             return false
         }
@@ -81,24 +52,37 @@ private fun List<CharArray>.wordExists(
         }
     }
 
-    println("found match! in direction $crawlDirection from ($startRowIndex,$startColumnIndex)")
+    // WIP clean up logs
+//    println("found match! in direction $crawlDirection from $startPosition")
     return true
 }
 
-enum class CrawlDirection {
-    UP,
-    UP_RIGHT,
-    RIGHT,
-    DOWN_RIGHT,
-    DOWN,
-    DOWN_LEFT,
-    LEFT,
-    UP_LEFT,
+enum class CrawlDirection(private val rowDelta: Int, private val columnDelta: Int) {
+    UP(rowDelta = -1, columnDelta = 0),
+    UP_RIGHT(rowDelta = -1, columnDelta = 1),
+    RIGHT(rowDelta = 0, columnDelta = 1),
+    DOWN_RIGHT(rowDelta = 1, columnDelta = 1),
+    DOWN(rowDelta = 1, columnDelta = 0),
+    DOWN_LEFT(rowDelta = 1, columnDelta = -1),
+    LEFT(rowDelta = 0, columnDelta = -1),
+    UP_LEFT(rowDelta = -1, columnDelta = -1),
+    ;
+
+    fun calculatePosition(startPosition: GridPosition, stepSize: Int): GridPosition {
+        return GridPosition(
+            row = startPosition.row + stepSize * rowDelta,
+            column = startPosition.column + stepSize * columnDelta
+        )
+    }
 }
 
-private fun part2(input: List<String>): Int {
+private fun part2(grid: List<CharArray>): Int {
+//    printMatrixWithCoordinates(grid) // WIP clean up
     return -1
 }
+
+
+data class GridPosition(val row: Int, val column: Int)
 
 private fun printMatrixWithCoordinates(grid: List<CharArray>) {
     grid.forEachIndexed { rowIndex, row ->
@@ -106,6 +90,5 @@ private fun printMatrixWithCoordinates(grid: List<CharArray>) {
             print("$rowIndex,$columnIndex:$letter  ")
         }
         print("\n")
-//        println("row $index: ${row.joinToString("")}")
     }
 }
