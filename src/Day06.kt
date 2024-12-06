3c3fd93a1424c4eb409a8dd9183f64f8509f5e85
Jeffrey Starke <dev@jeffreystarke.me>
2024-12-06 04:17:27 -0500

    Give up on kotlin notebook usage

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,13 +3,16 @@ fun main() {
     val realInput = readInput("Day06")
 
     println("[part 1] TEST result = ${part1(testInput)}")
-    println("[part 2] TEST result = ${part2(testInput)}\n")
+//    println("[part 2] TEST result = ${part2(testInput)}\n")
 
-    println("[part 1] result = ${part1(realInput)}")
-    println("[part 2] result = ${part2(realInput)}")
+//    println("[part 1] result = ${part1(realInput)}")
+//    println("[part 2] result = ${part2(realInput)}")
 }
 
 private fun part1(input: List<String>): Int {
+    val grid = Grid(input.map { it.toCharArray() })
+    val guardCoord = grid.indexOfFirst('<', 'v', '^', '>')
+    println("guard coord = $guardCoord --- orientation = ${guardCoord?.let { grid.getAt(it) }}")
     return -1
 }
 
