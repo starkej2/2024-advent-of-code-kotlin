4f5afc04c59738243b649e87816bebff58ce7a4d
Jeffrey Starke <dev@jeffreystarke.me>
2024-12-06 04:37:14 -0500

    Solve day 6 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,5 +1,8 @@
+import common.Coordinate
 import common.Grid
 import common.readInput
+
+private const val enableVerboseLogs = false
 
 fun main() {
     val testInput = readInput("Day06_test")
@@ -8,17 +11,147 @@ fun main() {
     println("[part 1] TEST result = ${part1(testInput)}")
 //    println("[part 2] TEST result = ${part2(testInput)}\n")
 
-//    println("[part 1] result = ${part1(realInput)}")
+    println("[part 1] result = ${part1(realInput)}")
 //    println("[part 2] result = ${part2(realInput)}")
 }
 
-private fun part1(input: List<String>): Int {
-    val grid = Grid(input.map { it.toCharArray() })
-    val guardCoord = grid.indexOfFirst('<', 'v', '^', '>')
-    println("guard coord = $guardCoord --- orientation = ${guardCoord?.let { grid.valueAt(it) }}")
-    return -1
+private fun part1(inputLines: List<String>): Int {
+    var map = LabMap(inputLines)
+    if (enableVerboseLogs) {
+        println("\ninitial map state:")
+        map.print()
+    }
+
+    val distinctPositionsVisited = buildSet {
+        do {
+            val nextMove = map.getNextGuardMove()
+            map = map.nextState(nextMove)
+            if (enableVerboseLogs) {
+                println("next state:")
+                map.print()
+            }
+            add(nextMove.oldPosition)
+        } while (nextMove !is GuardMove.ExitMap)
+    }
+
+    return distinctPositionsVisited.size
 }
 
 private fun part2(input: List<String>): Int {
     return -1
+}
+
+private data class LabMap(
+    val grid: Grid,
+) {
+    constructor(inputLines: List<String>) : this(Grid(inputLines.map { it.toCharArray() }))
+
+    val guardPosition: GuardPosition? = findGuardPosition()
+
+    fun print() = grid.print()
+
+    private fun findGuardPosition(): GuardPosition? {
+        return grid.indexOfFirst(GuardPosition.Orientation.symbols)?.let { position ->
+            GuardPosition(
+                coordinate = position,
+                orientation = GuardPosition.Orientation.fromSymbol(grid.valueAt(position))
+            )
+        }
+    }
+
+    fun getNextGuardMove(): GuardMove {
+        if (guardPosition == null) {
+            error("Unable to determine next guard position: guard not found")
+        }
+
+        val oldPosition = guardPosition.coordinate
+        val newPosition = when (guardPosition.orientation) {
+            GuardPosition.Orientation.UP -> oldPosition.copy(y = oldPosition.y - 1)
+            GuardPosition.Orientation.DOWN -> oldPosition.copy(y = oldPosition.y + 1)
+            GuardPosition.Orientation.LEFT -> oldPosition.copy(x = oldPosition.x - 1)
+            GuardPosition.Orientation.RIGHT -> oldPosition.copy(x = oldPosition.x + 1)
+        }
+
+        return when {
+            newPosition.x !in 0 until grid.width || newPosition.y !in 0 until grid.height -> {
+                GuardMove.ExitMap(oldPosition, newPosition)
+            }
+
+            grid.valueAt(newPosition) == '#' -> {
+                GuardMove.TurnRight(oldPosition, newPosition)
+            }
+
+            else -> {
+                GuardMove.GoForward(oldPosition, newPosition)
+            }
+        }
+    }
+
+    fun nextState(move: GuardMove): LabMap {
+        if (guardPosition == null) {
+            error("Unable to move guard: guard not found")
+        }
+
+        return when (move) {
+            is GuardMove.GoForward -> {
+                this.copy(
+                    grid = grid
+                        .setValueAt(guardPosition.coordinate, '.')
+                        .setValueAt(move.newPosition, guardPosition.orientation.symbol)
+                )
+            }
+
+            is GuardMove.TurnRight -> {
+                val newOrientation = when (guardPosition.orientation) {
+                    GuardPosition.Orientation.UP -> GuardPosition.Orientation.RIGHT
+                    GuardPosition.Orientation.DOWN -> GuardPosition.Orientation.LEFT
+                    GuardPosition.Orientation.LEFT -> GuardPosition.Orientation.UP
+                    GuardPosition.Orientation.RIGHT -> GuardPosition.Orientation.DOWN
+                }
+                this.copy(
+                    grid = grid.setValueAt(guardPosition.coordinate, newOrientation.symbol)
+                )
+            }
+
+            is GuardMove.ExitMap -> {
+                this.copy(
+                    grid = grid.setValueAt(guardPosition.coordinate, '.')
+                )
+            }
+        }
+    }
+}
+
+private data class GuardPosition(
+    val coordinate: Coordinate,
+    val orientation: Orientation,
+) {
+    enum class Orientation(val symbol: Char) {
+        UP('^'),
+        DOWN('v'),
+        LEFT('<'),
+        RIGHT('>');
+
+        companion object {
+            private val orientationBySymbol = mapOf(
+                '^' to UP,
+                'v' to DOWN,
+                '<' to LEFT,
+                '>' to RIGHT,
+            )
+            val symbols: Set<Char> = orientationBySymbol.keys
+
+            fun fromSymbol(symbol: Char): Orientation = orientationBySymbol[symbol]
+                ?: error("Invalid orientation symbol: $symbol")
+        }
+    }
+}
+
+sealed interface GuardMove {
+    val oldPosition: Coordinate
+    val newPosition: Coordinate
+
+    data class GoForward(override val oldPosition: Coordinate, override val newPosition: Coordinate) : GuardMove
+    data class TurnRight(override val oldPosition: Coordinate, override val newPosition: Coordinate) : GuardMove
+    data class ExitMap(override val oldPosition: Coordinate, override val newPosition: Coordinate) : GuardMove
 }
--- src/common/Grid.kt
+++ src/common/Grid.kt
@@ -1,6 +1,9 @@
 package common
 
 data class Grid(private val elements: List<CharArray>) {
+    val width = elements.maxBy { row -> row.size }.size
+    val height = elements.size
+
     val coordinates: Sequence<Coordinate> = sequence {
         for (rowIndex: Int in elements.indices) {
             for (columnIndex: Int in elements[rowIndex].indices) {
@@ -12,14 +15,28 @@ data class Grid(private val elements: List<CharArray>) {
     fun valueAt(x: Int, y: Int): Char = elements[y][x]
     fun valueAt(coordinate: Coordinate): Char = valueAt(coordinate.x, coordinate.y)
 
-    fun indexOfFirst(vararg char: Char): Coordinate? {
-        val charsToFind = char.toSet()
+    fun indexOfFirst(vararg char: Char): Coordinate? = indexOfFirst(char.toSet())
+
+    fun indexOfFirst(charsToFind: Set<Char>): Coordinate? {
         coordinates.forEach { coordinate ->
             if (charsToFind.contains(valueAt(coordinate))) {
                 return coordinate
             }
         }
         return null
+    }
+
+    fun setValueAt(coordinate: Coordinate, value: Char): Grid {
+        val newElements = elements.map { it.copyOf() }.toMutableList()
+        newElements[coordinate.y][coordinate.x] = value
+        return Grid(newElements)
+    }
+
+    fun print() {
+        println("  ${(0 until width).joinToString(" ")}")
+        elements.forEachIndexed { index, row ->
+            println("$index ${row.joinToString(" ")}")
+        }
     }
 }
 
